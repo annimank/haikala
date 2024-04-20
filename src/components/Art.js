d9119950d4c8446ee8f20788876c3ae4fec46d6a
annimank <mankki.anni@gmail.com>
2024-04-20 11:35:15 +0300

    Firebase and EmailJS

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,3 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, Card, CardContent, styled, useTheme, useMediaQuery } from '@mui/material';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage';
+import { Link } from 'react-router-dom';
+import config from '../Config';
+
+async function getImageURL(gsURL) {
+    const storage = getStorage(app);
+    const imageRef = storageRef(storage, gsURL);
+    const downloadURL = await getDownloadURL(imageRef);
+    return downloadURL;
+}
+
+const app = initializeApp(config);
+const database = getDatabase(app);
+
+function Art() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    //const isLarge = useMediaQuery(theme.breakpoints.down('lg'));
+
+    const LogoBox = styled(Box) ({
+        marginBottom: '50px',
+    });
+
+    const LogoLink = styled(Link)({
+        display: 'block',
+        margin: '0 auto',
+        width: '90%',
+        textAlign: 'center',
+        height: 'auto',
+    });
+
+    const ArtCard = styled(Card)({
+        width: isMobile ? '100%' : '80%',
+        marginBottom: '16px',
+        backgroundColor: theme.palette.background.default,
+    });
+    
+    const ArtCardContent = styled(CardContent)({
+        display: 'flex',
+        flex: 1,
+        flexDirection: isMobile ? 'column' : 'row',
+    });
+
+    //const ArtCardContent = styled(CardContent)(
+    //    ({ theme }) => ({
+    //      display: 'flex',
+    //      flex: 1,
+    //      flexDirection: 'row', // Default direction is row
+    //      [theme.breakpoints.down('sm')]: {
+    //        flexDirection: 'column', // Change direction to column on small screens
+    //      },
+    //      [theme.breakpoints.between('md', 'lg')]: {
+    //        flexDirection: 'row',
+    //      },
+          // Add more breakpoints and corresponding styles as needed
+    //    })
+    //  );
+    
+    const InfoDiv = styled('div')({
+        display: 'flex',
+        flex: isMobile ? 1 : 1,
+        flexDirection: 'column',
+        marginTop: isMobile ? '0px' : '20%',
+    });
+    
+    const ImgDiv = styled('div')({
+        display: 'flex',
+        flex: isMobile ? 3 : 1,
+        alignContent: 'center',
+    });
+    
+    const Img = styled('img')({
+        maxWidth: '100%',
+        height: 'auto',
+    });
+
+    const [artData, setArtData] = useState(null);
+
+    useEffect(() => {
+        const artRef = ref(database, 'art');
+        onValue(artRef, async (snapshot) => {
+            const data = snapshot.val();
+            const artworksWithHTTPSURLs = await Promise.all(Object.keys(data).map(async (artId) => {
+                const imageUrl = data[artId].imgURL;
+                const httpsURL = imageUrl.startsWith('gs://') ? await getImageURL(imageUrl) : imageUrl;
+                return { ...data[artId], imgURL: httpsURL };
+            }));
+
+            setArtData(artworksWithHTTPSURLs);
+        });
+    }, []);
+
+    // Check if artData not null
+    const sortedArtData = artData ? [...artData].sort((a, b) => new Date(b.date) - new Date(a.date)) : [];
+
+    return (
+        <Box>
+            <LogoBox>
+                {isMobile && (
+                    <LogoLink to="/Home">
+                        <img src="../assets/ogol.svg" alt="Logo" />
+                    </LogoLink>
+                )}
+            </LogoBox>
+            <Box display="flex" flexDirection="column" alignItems="center" sx={{ paddingTop: '60px', marginBottom: '100px' }}>
+                {sortedArtData.map((artwork) => (
+                    <ArtCard key={artwork.id} elevation={0}>
+                        <ArtCardContent>
+                            <InfoDiv>
+                                <Typography variant="h6">{artwork.name}</Typography>
+                                <Typography variant="body1">{artwork.date.slice(0, 4)}</Typography>
+                                <Typography variant="body1">{artwork.medium}</Typography>
+                            </InfoDiv>
+                            <ImgDiv>
+                                <Img src={artwork.imgURL} alt={artwork.name} />
+                            </ImgDiv>
+                        </ArtCardContent>
+                    </ArtCard>
+                ))}
+            </Box>
+        </Box>
+    );
+}
+
+export default Art;
+
+/*
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
@@ -12,3 +143,4 @@ function Art() {
 }
 
 export default Art;
+*/
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,174 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import SendIcon from '@mui/icons-material/Send';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { Box, Typography, styled, useTheme, useMediaQuery } from '@mui/material';
+import { Link } from "react-router-dom";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+function Contact() {
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const ContactBoxMain = styled(Box)({
+    display: 'flex',
+    flexDirection: isMobile ? 'column' : 'row',
+    justifyContent: 'center',
+    marginTop: isMobile ? '0px' : '80px',
+    marginBottom: isMobile ? '30px' : '500px',
+  });
+
+  const LogoBox = styled(Box)({
+    marginBottom: '50px',
+  });
+
+  const LogoLink = styled(Link)({
+    display: 'block',
+    margin: '0 auto',
+    width: '90%',
+    textAlign: 'center',
+    height: 'auto',
+  });
+
+  const ContactText = styled(Box)({
+    flex: 1,
+    marginBottom: '50px',
+    textAlign: 'center',
+  });
+
+  const Text = styled(Typography)({
+    margin: '20px',
+    fontSize: '1.375rem', // Convert pixel value to rem
+  });
+
+  const FormBox = styled(Box)({
+    flex: 2,
+  });
+
+  const Form = styled('form')({
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '80%',
+    margin: '0 auto',
+  });
+
+  const Label = styled('label')({
+    marginBottom: '5px',
+    fontWeight: 'bold',
+    fontSize: '1.125rem', // Convert pixel value to rem
+  });
+
+  const Input = styled('input')({
+    padding: '8px',
+    marginBottom: '10px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    fontSize: '0.875rem', // Convert pixel value to rem
+    outline: 'none',
+    backgroundColor: theme.palette.background.secondary,
+  });
+
+  const TextArea = styled('textarea')({
+    padding: '8px',
+    marginBottom: '10px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    fontSize: '0.875rem', // Convert pixel value to rem
+    height: '100px',
+    resize: 'vertical',
+    outline: 'none',
+    backgroundColor: theme.palette.background.secondary,
+  });
+
+  const Button = styled('button')({
+    padding: '10px 20px',
+    backgroundColor: '#DAD2BC',
+    color: '#252323',
+    borderWidth: '0px',
+    borderColor: '#252323',
+    borderRadius: '20px',
+    fontSize: '1rem', // Convert pixel value to rem
+    cursor: 'pointer',
+    outline: 'none',
+    marginRight: '5px',
+  });
+
+  const form = useRef();
+  const [open, setOpen] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(process.env.REACT_APP_serviceKey, process.env.REACT_APP_templateKey, form.current, {
+        publicKey: process.env.REACT_APP_publicKey,
+      })
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert('Your message has been sent!');
+          form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <ContactBoxMain>
+      <LogoBox>
+        {isMobile && (
+          <LogoLink to="/Home">
+            <img src="../assets/ogol.svg" alt="Logo" />
+          </LogoLink>
+        )}
+      </LogoBox>
+
+      <ContactText>
+        <Text>Please send an inquiry telling which artwork you are interested in, or if you have a custom work in mind.</Text>
+        <Text>Leave your name and email, so we can get back to you.</Text>
+      </ContactText>
+      <FormBox>
+        <Form ref={form} onSubmit={sendEmail}>
+          <Label>Name</Label>
+          <Input type="text" name="user_name" required />
+          <Label>Email</Label>
+          <Input type="email" name="user_email" required />
+          <Label>Message</Label>
+          <TextArea name="message" required />
+          <Button type="submit">
+            <SendIcon style={{ marginLeft: '5px' }} />
+          </Button>
+        </Form>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            Your message has been sent!
+          </Alert>
+        </Snackbar>
+      </FormBox>
+
+    </ContactBoxMain>
+  );
+};
+
+export default Contact;
+
+
+/*
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
@@ -12,3 +183,4 @@ function Contact() {
 }
 
 export default Contact;
+*/
